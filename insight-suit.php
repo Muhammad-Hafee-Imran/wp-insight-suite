89eb9a5105eb19bcebbde89149f85562695b4fb7
Muhammad Hafee Imran <mhafee.imran@gmail.com>
2025-08-12 18:11:14 +0500

    Initial commit

---
+++ insight-suit.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin name: Insight Suite.
+ * Description: A modern WordPress plugin that collects, analyzes, and displays user feedback and content engagement metrics.
+ * Version: 1.0.0
+ * Author: Muhammad Hafee Imran
+ */
+defined('ABSPATH') || exit;
